a70d52cf2d52e35d069ef090c233b2c275082740
Muhammad Umer <aabdullahaslam91@gmail.com>
2024-11-03 16:39:43 +0500

    Chapter 31 to 34

---
+++ app.js
@@ -0,0 +1,81 @@
+// QUESTION 1 START 
+// var todayDate = new Date();
+// console.log(todayDate);
+// QUESTION 1 END
+
+// QUESTION 2 START 
+
+// var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+// var todayDate = new Date();
+// var updatedDate = todayDate.getMonth()
+// var updatedDate1 = months[updatedDate]
+// console.log(updatedDate1);
+
+// QUESTION 2 END
+
+// QUESTION 3 START 
+// var months = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// var todayDate = new Date();
+// var updatedDate = todayDate.getDay()
+// var updatedDate1 = months[updatedDate]
+// alert(updatedDate1);
+
+// QUESTION 3 END
+
+
+
+// QUESTION 4 START 
+
+// var todayDate = new Date();
+// var updatedDate = todayDate.getDay()
+
+// if(updatedDate > 5 || updatedDate < 1){
+//     console.log("It's Fun day")
+// }else{
+//     console.log("It's working day");
+// }
+
+// QUESTION 4 END
+
+// QUESTION 5 START 
+
+// var todayDate = new Date();
+// var updatedDate = todayDate.getDate()
+
+// if(updatedDate <= 15){
+//     console.log("First Fifteen Date of month")
+// }else{
+//     console.log("Last Fifteen Date of month");   
+// }
+
+// QUESTION 5 END
+
+// QUESTION 6 START 
+
+
+// QUESTION 6 END
+
+// QUESTION 7 START 
+// QUESTION 7 END
+
+// QUESTION 8 START 
+// QUESTION 8 END
+
+// QUESTION 9 START 
+// QUESTION 9 END
+
+// QUESTION 10 START 
+// QUESTION 10 END
+
+// QUESTION 11 START 
+// QUESTION 11 END
+
+// QUESTION 12 START 
+// QUESTION 12 END
+
+// QUESTION 13 START 
+// QUESTION 13 END
+
+// QUESTION 14 START 
+// QUESTION 14 END
+
